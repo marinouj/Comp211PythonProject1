b26739dfcc543b4b8c825fc51d4e9686f2c7cfb4
marinouj <marinoujoanna@gmail.com>
2019-03-23 18:01:18 +0200

    Part3b finished

--- try.py
+++ try.py
@@ -1,11 +1,12 @@
 import random
 import array
-buffer_size = 2
-file_size = 8
-sorted_file_size = 4
+buffer_size = 1000
+file_size = 100000
+sorted_file_size = 10000
 page_size = buffer_size*4
-maximum_random_number = 10
+maximum_random_number = 100000
 sorted_file_number = int(file_size/sorted_file_size)
+final_sorted_name = "finalSortedFile.txt"
 
 def main():
     disk_access = 0
@@ -24,7 +25,9 @@ def main():
     print("Disk accesses required to create file: ", disk_access)
     sort()
     final_sort()
+    #print_file(final_sorted_name)
     serial_search()
+    binary_search()
 
 
 """The sort algorithm is used in order to sort the random generated file into many seperate once.
@@ -80,7 +83,7 @@ def final_sort():
     buffer_pointer = [0 for i in range(sorted_file_number + 1)]
     sorted_buffer = [[0 for y in range(buffer_size)]for x in range(sorted_file_number + 1)]
     column = [0 for x in range(sorted_file_number)]
-
+    final_file = open(final_sorted_name, "wb")
     for i in range(sorted_file_number):
         sorted_buffer[i], file_pointer[i] = get_next_page(file_pointer[i], generate_file_name(i))
     while file_pointer[sorted_file_number] != file_size*4:
@@ -89,17 +92,16 @@ def final_sort():
         position = minimum(column, empty_buffer)
         sorted_buffer[sorted_file_number][buffer_pointer[sorted_file_number]] = \
             sorted_buffer[position][buffer_pointer[position]]
-        buffer_pointer[position] = buffer_pointer[position] + 1
-        buffer_pointer[sorted_file_number] = buffer_pointer[sorted_file_number] + 1
+        buffer_pointer[position] += 1
+        buffer_pointer[sorted_file_number] += 1
 
         if file_ended[position] and (buffer_pointer[position] == buffer_size):
             empty_buffer[position] = True
             buffer_pointer[position] = buffer_size - 1
 
         if buffer_pointer[sorted_file_number] == buffer_size:
-            #print(sorted_buffer[sorted_file_number])
             file_pointer[sorted_file_number] = \
-                write_next_page(file_pointer[sorted_file_number], sorted_buffer[sorted_file_number], "finalSortedFile.txt")
+                write_next_page(file_pointer[sorted_file_number], sorted_buffer[sorted_file_number], final_file)
             buffer_pointer[sorted_file_number] = 0
 
         if buffer_pointer[position] == buffer_size:
@@ -107,6 +109,14 @@ def final_sort():
                 get_next_page(file_pointer[position], generate_file_name(position))
             buffer_pointer[position] = 0
             file_ended[position] = (file_pointer[position] == sorted_file_size*4)
+    final_file.close()
+
+
+def print_file(filename):
+    fp = 0
+    while fp < file_size*4:
+        buffer, fp = get_next_page(fp, filename)
+        print(buffer)
 
 
 """Algorithm used to search serially 40 random numbers in the sorted file"""
@@ -114,18 +124,18 @@ def serial_search():
     disk_access = [0 for i in range(40)]
     for k in range(40):
         key = random.randint(0, maximum_random_number)
-        disk_access[k] = search_in_file_serial("finalSortedFile.txt", key)
-    print(average(disk_access))
+        disk_access[k] = search_in_file_serial(key)
+    print("Average disk accesses required for the serial search", average(disk_access))
 
 
 """Algorithm used to search serially each random number in the sorted file"""
-def search_in_file_serial(final_file, key):
+def search_in_file_serial(key):
     disk_access = 0
     successful_search = False
     file_pointer = 0
     while file_pointer != file_size * 4 and not successful_search:
-        disk_access = disk_access + 1
-        buffer, file_pointer = get_next_page(file_pointer, final_file)
+        disk_access += 1
+        buffer, file_pointer = get_next_page(file_pointer, final_sorted_name)
         successful_search = search_number(buffer, key)
     return disk_access
 
@@ -138,6 +148,62 @@ def search_number(buffer, key):
     return False
 
 
+"""Algorithm used to search binary 40 random numbers in the sorted file"""
+def binary_search():
+    disk_access = [0 for i in range(10)]
+    num_of_pages = file_size/buffer_size
+    file_pointer = num_of_pages*buffer_size*4/2
+    for k in range(10):
+        key = random.randint(0, maximum_random_number)
+        disk_access[k] = search_in_file_binary(file_pointer, num_of_pages/2, key)
+        #print(disk_access[k])
+    print("Average disk accesses needed for the binary search: ", average(disk_access))
+
+
+"""This recursive function reads the page of the file that corresponds to
+    the file pointer, searches for the key in the page."""
+def search_in_file_binary(fp, l, key):
+    length = l
+    file_pointer = int(fp)
+    disk_access = 0
+    buffer, f_pointer = get_next_page(file_pointer, final_sorted_name)
+    disk_access += 1
+    move = search_in_buffer(buffer, key)
+    if move == "Found":
+        #print(key, move)
+        return disk_access
+    elif move == "Not Found":
+        #print(key, move)
+        return disk_access
+    elif move == "Left":
+        if file_pointer == buffer_size*4:
+            return disk_access + search_in_file_binary(0, length/2, key)
+        file_pointer -= length*buffer_size*2
+        length = length/2
+    else:
+        file_pointer += length*buffer_size*2
+        length = length / 2
+    if 0 <= file_pointer <= file_size*4 - 1 and length >= 1:
+        disk_access += search_in_file_binary(file_pointer, length, key)
+    return disk_access
+
+
+"""This function returns found or not found if the key is a number between buffer[0] and
+    buffer[buffer_size - 1]. Else it removes whether it is placed in previous or next pages
+    of the file"""
+def search_in_buffer(buffer, key):
+    if buffer[0] <= key <= buffer[buffer_size-1]:
+        if search_number(buffer, key):
+            return "Found"
+        else:
+            return "Not found"
+    elif buffer[0]>key:
+        return "Left"
+    else:
+        return "Right"
+
+
+"""Finds and returns the average of an array"""
 def average(arr):
     s = 0
     for i in range(len(arr)):
@@ -145,9 +211,11 @@ def average(arr):
     return s/len(arr)
 
 
+"""This function finds the position of minimum value of an array.
+    It also excludes from the column array the nodes whose corresponding has finished node is true"""
 def minimum(column, has_finished):
     x = 0
-    while x in range(sorted_file_number) and has_finished[x]:
+    while x in range(sorted_file_number) and has_finished[x]: #choosing the starting point of our search for the min skipping the has finished nodes
         x = x + 1
     min = column[x]
     pos = x
@@ -158,20 +226,22 @@ def minimum(column, has_finished):
     return pos
 
 
+"""This function is generating names for the sorted files"""
 def generate_file_name(i):
     return "filename" + str(i) + ".txt"
 
 
-def write_next_page(fp, buffer, filename):
-    file = open(filename, "wb")
+"""We use this function to write a page of a file"""
+def write_next_page(fp, buffer, final_file):
+    file = final_file
     page = array.array("L", buffer).tobytes()
     file.seek(fp)
     file.write(page)
     file_pointer = file.tell()
-    file.close()
     return file_pointer
 
 
+"""We use this function to read a page of a file"""
 def get_next_page(fp, filename):     #loading new page from sorted files when needed and turning it into int
     sorted_file = open(filename, "rb")
     sorted_file.seek(fp)
